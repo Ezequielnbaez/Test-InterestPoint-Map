fff9e0c9e387306211515da2246dcdf15c0ebd5b
Ezequielnbaez <ezequielnbaez@gmail.com>
2022-04-25 13:46:34 -0300

    BasicLayer
    Agregando layer básica y su formulario, con las const que se utilizarán.

--- js/script.js
+++ js/script.js
@@ -28,4 +28,86 @@ require(["esri/config",
     zoom: 16
   });
 
+  const point = {
+    type: "point",
+    longitude: -58.44861477597812,
+    latitude: -34.59090359667831
+  };
+
+  const simpleMarkerSymbol = {
+    type: "simple-marker",
+    color: [226, 119, 40],
+    outline: {
+      color: [255, 255, 255],
+      width: 1
+    }
+  };
+  const myLayer = new FeatureLayer({
+
+    title: "Points",
+    fields: [{
+        name: "ObjectID",
+        alias: "ObjectID",
+        type: "oid"
+      },
+      {
+        name: "Nombre",
+        alias: "Nombre",
+        type: "string"
+      },
+      {
+        name: "Cat",
+        alias: "Cat",
+        type: "string"
+      },
+      {
+        name: "Dir",
+        alias: "Dir",
+        type: "string"
+      },
+      {
+        name: "Tel",
+        alias: "Tel",
+        type: "string"
+      },
+      {
+        name: "X",
+        alias: "X",
+        type: "double"
+      },
+      {
+        name: "Y",
+        alias: "Y",
+        type: "double"
+      }
+    ],
+    objectIdField: "ObjectID",
+    geometryType: "point",
+    spatialReference: {
+      wkid: 4326
+    },
+    source: [],
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "web-style",
+        styleName: "Esri2DPointSymbolsStyle",
+        name: "flag"
+      }
+    },
+    popupTemplate: {
+      title: "{Nombre}",
+      content: "<ul><li><b>Dirección:</b>{Dir} </li>" +
+        "<li><b>Teléfono:</b>{Tel} </li>" +
+        "<li><b>(X,Y):</b>{X},{Y} </li>" +
+        "<li><b>Categoría:</b>{Cat} </li><ul>"
+    }
+  });
+  map.add(myLayer);
+  const addBtn = document.getElementById("add");
+  const form = document.getElementById('form');
+  const removeBtn = document.getElementById("remove");
+
+  view.ui.add(document.getElementById("form"), "top-right");
+
 });
